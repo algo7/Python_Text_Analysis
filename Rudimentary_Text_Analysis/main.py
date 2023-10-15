f0f91944828934ee5641c3a86ea4042ae2b88264
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-15 18:20:41 +0200

    add data exploration steps

---
+++ Rudimentary_Text_Analysis/main.py
@@ -0,0 +1,27 @@
+# Libraries
+import pandas as pd
+import nltk
+from nltk.stem import snowball
+from nltk.tokenize import word_tokenize as wt
+from nltk.corpus import stopwords
+
+# Download nltk components
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Read the data from a csv file
+data = pd.read_csv('https://raw.githubusercontent.com/algo7/Python_Text_Analysis/main/Data/Hotel_Alexander.csv')
+
+# Explore the data
+## Print the column names
+data.columns
+
+## List the first 5 rows
+data.head(n=5)
+
+## Print the shape/dimension of the data
+data.shape
+
+## Get basic information about the data
+data.info()
+
